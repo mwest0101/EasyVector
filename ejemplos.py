658690429886f380ef394796fb4805f9c537e279
mwest0101 <mauriciowest@gmail.com>
2025-06-04 18:51:37 -0300

    =Finale Version 1

---
+++ ejemplos.py
@@ -0,0 +1,18 @@
+from EasyVector import *
+	# 	print(self)
+vec=Vector.Int(3,2,2)
+vec[1][0]=5
+print(vec)
+
+vec=Vector.Char(2,2,2)
+vec[1][0]='c'
+print(vec)
+
+vec=Vector.Float(2,2,2)
+vec[1][0]=0.0
+print(vec)
+
+vec=Vector.String(2,2,2)
+vec[0][0]="Hola"
+vec[1][1]="Mundo"
+print(vec)
